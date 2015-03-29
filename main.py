18cb799da03aa883e68b49010d3422caa9f7808c
Michael Van Veen <michael@mvanveen.net>
2015-03-29 13:03:01 -0700

    lincoln likes !=

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import base64
 def convert_hex_to_base64(hex_str):
     if not isinstance(hex_str, basestring):
         raise TypeError("should have provided str! :(")
+
     hex_str = hex_str.decode("hex")
-    
     return base64.standard_b64encode(bytes(hex_str))
 
 
@@ -14,7 +14,7 @@ def fixed_xor(s1, s2):
     s1 = [int(c, 16) for c in s1]
     s2 = [int(c, 16) for c in s2]
 
-    if not len(s1) == len(s2):
+    if len(s1) != len(s2):
         raise ValueError("strings are of not same length :(")
 
     zipped_result = zip(s1, s2)
